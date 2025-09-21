8de60e1f5cffc86d82e2f6b79e0627c9e4fc8f89
Atomiq2016 <Atomiq2016@gmail.com>
2025-09-21 16:05:52 +0100

    Initial Commit

---
+++ LocalChat/LocalChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatApp.swift
+//  LocalChat
+//
+//  Created by Admin on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct LocalChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LocalChatTests/LocalChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  LocalChatTests.swift
+//  LocalChatTests
+//
+//  Created by Admin on 9/21/25.
+//
+
+import Testing
+@testable import LocalChat
+
+struct LocalChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
